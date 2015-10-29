e289e50d26b5480105716866c2f91700701051a8
danielfriis <d@friis.me>
2015-10-29 22:35:42 +0100

    initial work on frontpage mobile

--- app/assets/javascripts/initialize_slick.js
+++ app/assets/javascripts/initialize_slick.js
@@ -6,6 +6,10 @@ $(document).on('ready page:load', function () {
 	  arrows: false,
 	  dots: false,
 	  initialSlide: -1,
-	  lazyLoad: 'ondemand',
+	  lazyLoad: 'ondemand'
+	});
+	$('body').on('click', '.slick-slide:not(.slick-active)', function(){
+		$('.slick-variable-width').slick('slickNext');
+		return false
 	});
 });
