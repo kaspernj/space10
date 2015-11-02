a45dcdb5f9b849a37ac722341b548b435fd79c80
danielfriis <d@friis.me>
2015-11-02 12:42:08 +0100

    minor fixes for mobile

--- app/assets/javascripts/page_load.js
+++ app/assets/javascripts/page_load.js
@@ -2,6 +2,8 @@ $(document).on('ready page:change', function () {
 	$('#svg_logo').show();
 	$('#gif_logo').hide();
 }).on('ready page:fetch', function () {
-	$('#svg_logo').hide();
-	$('#gif_logo').show();
+  if( $('#svg_logo').is(':visible') ) {
+    $('#svg_logo').hide();
+    $('#gif_logo').show();
+  }
 });
