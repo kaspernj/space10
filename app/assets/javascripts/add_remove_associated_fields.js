4fb21505490b4afe133ac56823f543c8513dbc7e
danielfriis <d@friis.me>
2015-11-04 10:02:20 +0100

    finishing initial work on footer

--- app/assets/javascripts/add_remove_associated_fields.js
+++ app/assets/javascripts/add_remove_associated_fields.js
@@ -1,17 +1,15 @@
 $(document).on('ready page:load', function () {
-
-		$('form').on('click', '.remove_fields', function (event) {
-		  $(this).prevAll('input.destroy').first().val('1');
-		  $(this).closest('fieldset').hide();
-		  event.preventDefault();
-		});
-
-		$('form').on('click', '.add_fields', function (event) {
-		  time = new Date().getTime();
-		  regexp = new RegExp($(this).data('id'), 'g');
-		  $(this).closest('.add_fields_here').before($(this).data('fields').replace(regexp, time));
-		  init_sortable();
-		  init_summernote();
-		  event.preventDefault()
-		});
+	$('form').on('click', '.remove_fields', function (event) {
+	  $(this).prevAll('input.destroy').first().val('1');
+	  $(this).closest('fieldset').hide();
+	  event.preventDefault();
+	});
+	$('form').on('click', '.add_fields', function (event) {
+	  time = new Date().getTime();
+	  regexp = new RegExp($(this).data('id'), 'g');
+	  $(this).closest('.add_fields_here').before($(this).data('fields').replace(regexp, time));
+	  init_sortable();
+	  init_summernote();
+	  event.preventDefault()
+	});
 });
