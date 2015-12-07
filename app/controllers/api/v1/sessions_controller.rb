0ec755a4399f9f96e6b0a031ccc651054e785da5
danielfriis <d@friis.me>
2015-12-07 18:20:33 +0100

    Fix minor typo bug in api sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::SessionsController < Api::V1::ApiController
 		@user = User.find_by(email: params[:email])
 		if @user && @user.authenticate(params[:password])
 			@user
-		else @user
+		else
       render json: { errors: ["User not found or email/password combination was wrong"] }, status: 400
     end
 	end
