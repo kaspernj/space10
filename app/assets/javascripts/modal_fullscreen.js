8cd9bed6621a76dd652e39a5af2f2b36364d1545
danielfriis <d@friis.me>
2015-12-19 12:51:19 +0100

    Update search for mobile

--- app/assets/javascripts/modal_fullscreen.js
+++ app/assets/javascripts/modal_fullscreen.js
@@ -2,22 +2,29 @@ $(document).on('ready page:load', function () {
   $("a[data-toggle='modal']").on('click', function(){
     $(this).addClass('modal-toggle-pressed');
   });
-  $(".modal-fullscreen").on('show.bs.modal', function () {
+  $(".modal").on('show.bs.modal', function () {
+    var curModal;
+    curModal = this;
+    $(".modal").each(function() {
+      if (this !== curModal) {
+        $(this).modal("hide");
+      }
+    });
     var color = $(this).data('backdrop-color') || 'white'
     setTimeout( function() {
       $(".modal-backdrop").addClass("modal-backdrop-fullscreen modal-backdrop-" + color);
     }, 0);
   });
-  $(".modal-fullscreen").on('hide.bs.modal', function () {
+  $("#modal-navigate").on('hide.bs.modal', function () {
     $("a[data-toggle='modal']").removeClass('modal-toggle-pressed');
   });
-  $(".modal-fullscreen").on('hidden.bs.modal', function () {
-    var color = $(this).data('backdrop-color') || 'white'
-    $(".modal-backdrop").removeClass("modal-backdrop-fullscreen modal-backdrop-" + color);
-  });
-  $('.modal').on('hidden.bs.modal', function(event){
-    setTimeout(function(){
-      $('[data-toggle="modal"]').blur();
-    });
-  });
+  // $(".modal-fullscreen").on('hidden.bs.modal', function () {
+  //   var color = $(this).data('backdrop-color') || 'white'
+  //   $(".modal-backdrop").removeClass("modal-backdrop-fullscreen modal-backdrop-" + color);
+  // });
+  // $('.modal').on('hidden.bs.modal', function(event){
+  //   setTimeout(function(){
+  //     $('[data-toggle="modal"]').blur();
+  //   });
+  // });
 });
