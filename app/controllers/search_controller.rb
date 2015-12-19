5cd6b02730164ccd0563b10bead10d4580033328
danielfriis <d@friis.me>
2015-12-19 13:59:03 +0100

    reject non-published search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,6 +6,7 @@ class SearchController < ApplicationController
 
   def search_results
     @search_results = PgSearch.multisearch(params[:search][:query]).with_pg_search_rank.map(&:searchable)
+    @search_results.reject!{|s| !s.published? || s.publish_at > Time.zone.now }
     if params[:search][:sort_by] == 'recent'
       @search_results.sort!{|a,b| b.publish_at <=> a.publish_at} if @search_results.any?
     end
