414f6879e56e9f633665689b8f4a453e24dcb8bf
danielfriis <d@friis.me>
2015-12-19 11:23:12 +0100

    More work on search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,6 +6,9 @@ class SearchController < ApplicationController
 
   def search_results
     @search_results = PgSearch.multisearch(params[:search][:query]).with_pg_search_rank.map(&:searchable)
+    if params[:search][:sort_by] == 'recent'
+      @search_results.sort!{|a,b| b.publish_at <=> a.publish_at} if @search_results.any?
+    end
     respond_to do |format|
       format.html
       format.js
