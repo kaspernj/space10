653cf092e16cadd0a13fa1d3f262183417ff797c
danielfriis <d@friis.me>
2015-12-20 09:33:38 +0100

    finishing video featured

--- app/assets/javascripts/video_embed.js
+++ app/assets/javascripts/video_embed.js
@@ -1,9 +1,29 @@
 $(document).on('ready page:load', function () {
-	$('.video-play-button').click(function(e){
-		var container = $(this).closest('.video-container');
-		container.find('.video-poster').hide();
-		var src = container.find('.video-embed').data('embed-src')
-		container.find('.video-embed').show().find('iframe').attr('src', src);
-		return false
-	})
+  $('body').on('click', '.video-play-button', function () {
+    var clickedContainer = $(this).closest('.video-container');
+
+    // Reset
+    $('.video-container').each(function(){
+      $(this).find('.video-poster').show();
+      $(this).find('.video-embed').hide().find('iframe').attr('src', '');
+    });
+
+    var largeContainer = $('.video-container-large');
+    var mq = window.matchMedia( "(min-width: 500px)" );
+
+    if (clickedContainer != largeContainer && mq.matches) {
+      var largeHtml = largeContainer.html();
+      var smallHtml = clickedContainer.html();
+
+      largeContainer.html(smallHtml);
+      clickedContainer.html(largeHtml);
+
+      clickedContainer = largeContainer;
+    }
+    clickedContainer.find('.video-poster').hide();
+    var src = clickedContainer.find('.video-embed').data('embed-src')
+    clickedContainer.find('.video-embed').show().find('iframe').attr('src', src);
+
+    return false;
+	});
 });
