a271f2f1e314fceb331e8f5af618865ac3376358
danielfriis <d@friis.me>
2016-01-19 16:13:38 +0100

    Bring back page transistions

--- app/assets/javascripts/page_transistions.js
+++ app/assets/javascripts/page_transistions.js
@@ -10,3 +10,11 @@
 //     $('#gif_logo').show();
 //   }
 // });
+$(document).on('ready page:change', function () {
+  $('body').removeClass('animated fadeOut')
+  $('body').addClass('animated fadeIn')
+});
+$(document).on('ready page:fetch', function () {
+  $('body').removeClass('animated fadeIn')
+  $('body').addClass('animated fadeOut')
+});
