2f606603826cc15a8ff4988aa89a9a5e845697d8
danielfriis <d@friis.me>
2016-01-20 22:01:18 +0100

    update page transition to accomodate better mobile experience

--- app/assets/javascripts/page_transistions.js
+++ app/assets/javascripts/page_transistions.js
@@ -5,18 +5,24 @@ $(document).on('ready page:fetch', function () {
   // }
   var src = $('#gif_logo').attr('src');
   $('#gif_logo').attr('src', src);
-  $('#page_content').removeClass('animated fadeIn')
-  $('#page_content').addClass('animated fadeOut')
-  $('.navbar-fixed-bottom').removeClass('animated fadeIn')
-  $('.navbar-fixed-bottom').addClass('animated fadeOut')
+  if ($('.navbar-fixed-bottom').is(':visible')) {
+    $('body').removeClass('animated fadeIn')
+    $('body').addClass('animated fadeOut')
+  } else {
+    $('#page_content').removeClass('animated fadeIn')
+    $('#page_content').addClass('animated fadeOut')
+  }
 });
 $(document).on('ready page:change', function () {
   // if ($('#gif_logo').is(':visible')) {
   //   $('#gif_logo').hide();
   //   $('#svg_logo').show();
   // }
-  $('#page_content').removeClass('animated fadeOut')
-  $('#page_content').addClass('animated fadeIn')
-  $('.navbar-fixed-bottom').removeClass('animated fadeOut')
-  $('.navbar-fixed-bottom').addClass('animated fadeIn')
+  if ($('.navbar-fixed-bottom').is(':visible')) {
+    $('body').removeClass('animated fadeOut')
+    $('body').addClass('animated fadeIn')  
+  } else {
+    $('#page_content').removeClass('animated fadeOut')
+    $('#page_content').addClass('animated fadeIn')
+  }
 });
