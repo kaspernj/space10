8526d5643c0892ff904a8a91f9ed22d2ffc73909
danielfriis <d@friis.me>
2016-01-23 11:52:01 +0100

    Update with the smaller gif logo

--- app/assets/javascripts/page_transistions.js
+++ app/assets/javascripts/page_transistions.js
@@ -5,12 +5,19 @@ $(document).on('ready page:fetch', function () {
   // }
   var src = $('#gif_logo').attr('src');
   $('#gif_logo').attr('src', src);
-  if ($('.navbar-fixed-bottom').is(':visible')) {
-    $('body').removeClass('animated fadeIn')
-    $('body').addClass('animated fadeOut')
-  } else {
-    $('#page_content').removeClass('animated fadeIn')
-    $('#page_content').addClass('animated fadeOut')
+  var src = $('#shrink_logo').attr('src');
+  $('#shrink_logo').attr('src', src);
+  var src = $('#mobile_logo').attr('src');
+  $('#mobile_logo').attr('src', src);
+  
+  $('#page_content').removeClass('animated fadeIn')
+  $('#page_content').addClass('animated fadeOut')
+
+  if ($('.navbar-fixed-bottom').is(':visible')) {  
+    $('#modal-navigate').removeClass('animated fadeIn')
+    $('#modal-navigate').addClass('animated fadeOut')
+    $('.navbar-fixed-bottom').removeClass('animated fadeIn')
+    $('.navbar-fixed-bottom').addClass('animated fadeOut')
   }
 });
 $(document).on('ready page:change', function () {
@@ -18,11 +25,13 @@ $(document).on('ready page:change', function () {
   //   $('#gif_logo').hide();
   //   $('#svg_logo').show();
   // }
-  if ($('.navbar-fixed-bottom').is(':visible')) {
-    $('body').removeClass('animated fadeOut')
-    $('body').addClass('animated fadeIn')  
-  } else {
-    $('#page_content').removeClass('animated fadeOut')
-    $('#page_content').addClass('animated fadeIn')
+  $('#page_content').removeClass('animated fadeOut')
+  $('#page_content').addClass('animated fadeIn')
+
+  if ($('.navbar-fixed-bottom').is(':visible')) {  
+    $('#modal-navigate').removeClass('animated fadeOut')
+    $('#modal-navigate').addClass('animated fadeIn')
+    $('.navbar-fixed-bottom').removeClass('animated fadeOut')
+    $('.navbar-fixed-bottom').addClass('animated fadeIn')
   }
 });
